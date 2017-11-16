88d0a7ee2148b75faab9fff230c0eaed9abb8db8
A.Fahy <alanfahy87@alans-air-2.nuigalway.ie>
2017-11-16 09:33:36 +0000

    Finished assignment

--- assignment.pl
+++ assignment.pl
@@ -1,7 +1,16 @@
 %Definitions for isElementInList(El, List)
-
+isElementInList(X, [X|_]).
+isElementInList(X,[_|T]):-
+isElementInList(X,T).
 %Definitions for reverseList(List, ReversedList)
-
-%insertElementIntoListEnd(El, List, NewList)
-
+reverseList([],[]).
+reverseList([X|Xrev],Rev) :-
+reverseList(Xrev,Yrev), mergeLists(Yrev,[X],Rev).
+%Definitions for insertElementIntoListEnd(El, List, NewList)
+insertElementIntoListEnd(X,[],[X]).
+insertElementIntoListEnd(X,[Y|Tail],[Y|Tail1]):-
+insertElementIntoListEnd(X,Tail,Tail1).
 %Definitions for mergeLists(List1, List2, Merged)
+mergeLists([],List,List).
+mergeLists([H|T],List,[H|M]):-
+mergeLists(T,List,M).
